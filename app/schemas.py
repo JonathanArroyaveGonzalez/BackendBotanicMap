2e873bc946ac8a6c8c9a9cfba6ee0b3721a4a4e5
JonathanArroyaveGonzalez <arroyave3000g@gmail.com.com>
2024-11-03 22:07:36 -0500

    feat: add Pydantic schemas for POI, Flora, and Fauna

---
+++ app/schemas.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+
+class POIBase(BaseModel):
+    nombre: str
+    descripcion: str
+    foto_url: str
+    tipo: str
+    longitud: str
+    latitud: str
+
+class POICreate(POIBase):
+    pass
+
+class POI(POIBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class FloraBase(BaseModel):
+    nombre_cientifico: str
+    familia: str
+
+class FloraCreate(FloraBase):
+    pass
+
+class Flora(FloraBase):
+    id: int
+    poi_id: int
+
+    class Config:
+        orm_mode = True
+
+class FaunaBase(BaseModel):
+    especie: str
+    nombre_comun: str
+    habitat: str
+
+class FaunaCreate(FaunaBase):
+    pass
+
+class Fauna(FaunaBase):
+    id: int
+    poi_id: int
+
+    class Config:
+        orm_mode = True
