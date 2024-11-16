ba06192c43bf599d73f4c2b597178e6056909650
JonathanArroyaveGonzalez <arroyave3000g@gmail.com.com>
2024-11-16 17:22:16 -0500

    fix: improve session handling in database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 import os,ssl
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
 # Cargar variables de entorno
@@ -9,10 +8,10 @@ load_dotenv()
 ssl_context = ssl.create_default_context()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"ssl_context": ssl_context},
-)
+#SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_TEST")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"ssl_context": ssl_context},)
+
 #engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
