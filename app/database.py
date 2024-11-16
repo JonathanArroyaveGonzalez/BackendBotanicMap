c7d697b459c97c728b06bcbfd3d69526f6b4b0ed
JonathanArroyaveGonzalez <arroyave3000g@gmail.com.com>
2024-11-15 22:27:11 -0500

    fix(database): migrate from SQLite to PostgreSQL
    - Updated database configuration to use PostgreSQL instead of SQLite.
    - Integrated pg8000 as the database driver for PostgreSQL.
    - Added SSL context for secure connections to the Neon database.
    - Removed SQLite-specific configurations.

--- app/database.py
+++ app/database.py
@@ -1,10 +1,19 @@
+import os,ssl
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./jardin_botanico.db"
+# Cargar variables de entorno
+load_dotenv()
+ssl_context = ssl.create_default_context()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"ssl_context": ssl_context},
+)
+#engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
